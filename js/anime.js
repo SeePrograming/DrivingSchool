995a0f725507af934debbb80c4e38df6860724cf
Sagar Vilasrao Shelke <sagarshelke18@gmail.com>
2018-02-06 12:29:13 +0530

    Add files via upload

---
+++ js/anime.js
@@ -0,0 +1,53 @@
+ window.sr = ScrollReveal();
+ sr.reveal('#herop', {
+     duration: 2000,
+     origin: 'bottom'
+ });
+
+sr.reveal('#onep', {
+     duration: 2000,
+     origin: 'top',
+     distance:'300px'
+ });
+
+sr.reveal('#onep2', {
+     duration: 2000,
+     origin: 'bottom',
+    distance:'300px'
+ });
+
+sr.reveal('.two1', {
+     duration: 2000,
+     origin: 'left',
+    distance:'300px'
+ });
+
+sr.reveal('.two2', {
+     duration: 2000,
+     origin: 'top',
+    distance:'300px'
+ });
+
+sr.reveal('.two3', {
+     duration: 2000,
+     origin: 'right',
+    distance:'300px'
+ });
+
+sr.reveal('.three1', {
+     duration: 2000,
+     origin: 'left',
+    distance:'300px'
+ });
+
+sr.reveal('.three2', {
+     duration: 2000,
+     origin: 'bottom',
+    distance:'300px'
+ });
+
+sr.reveal('.three3', {
+     duration: 2000,
+     origin: 'right',
+    distance:'300px'
+ });
